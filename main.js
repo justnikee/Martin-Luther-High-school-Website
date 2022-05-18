59211dba5ad7dae80235f1639fc877e172c24917
Nikeeyyy <68065805+justnikee@users.noreply.github.com>
2022-05-18 12:41:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,15 @@ closeBtn.addEventListener("click", ()=>{
 btn.addEventListener('click', ()=>{
     searchBox.style.display = "block"
 })
+
+//  hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
